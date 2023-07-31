163163310b483fab6d9f907d8591de5ce96759c0
B3njMcC <231186@virtualwindow.co.za>
2023-07-31 17:39:58 +0200

    2023/07/31
    add more pictures and jquery

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,40 @@
+const arrPlants = [
+  {
+    name: "Ficus Tree",
+    price: 320, 
+    description: "lorem ipsum",
+    image: "Plant 1.png"
+  },
+
+  {
+    name: "White Sprite Succulent",
+    price: 200, 
+    description: "lorem ipsum",
+    image: "Plant 2.png"
+  },
+
+  {
+    name: "Snake Plant",
+    price: 400, 
+    description: "lorem ipsum",
+    image: "Plant 3.png"
+  },
+
+  {
+    name: "Parlor Palm",
+    price: 350, 
+    description: "lorem ipsum",
+    image: "Plant 4.png"
+  },
+  {
+    name: "Parlor Palm",
+    price: 350, 
+    description: "lorem ipsum",
+    image: "Plant 5.png"
+  }
+]
+
+
 // When the document loads
 $(document).ready(function(){
 
@@ -12,10 +49,39 @@ $(document).ready(function(){
     // -----------------------------------------
     // Browse Page
 
+
+
+
+    
+
+// Load Plants
+
+
+// -----------------------------------------
+
     // Hide all description text from the plant cards
     $("#descriptionText").hide();
 
-  }); 
+    
+    // -----------------------------------------
+    //Load All Plants
+    // -----------------------------------------
+
+  
+
+      for (let i = 0; i < arrPlants.length; i++) {
+        let plant = arrPlants[i];
+        
+        console.log(plant);
+
+      }
+        //1: Set 
+      
+
+     
+
+
+// -----------------------------------------
 
   // When the card is clicked
   $(".card").click(function(){
@@ -28,3 +94,8 @@ $(document).ready(function(){
     $(".card-img-top").toggleClass("small");
 
   });
+
+  // -----------------------------------------
+  //Wishlist page
+  
+})
